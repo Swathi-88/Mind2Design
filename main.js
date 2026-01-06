d67b76cd1837b31c2a15fcb6d3f99932f7f7a1fa
Swathi-88 <swathigandhi2006@gmail.com>
2026-01-06 22:12:03 +0530

    Initial web app setup with Vite

---
+++ main.js
@@ -0,0 +1,7 @@
+import './style.css'
+
+document.querySelector('#cta-button').addEventListener('click', () => {
+  alert('Welcome to Mind2Design! Let\'s build something great.')
+})
+
+console.log('Mind2Design Web App Initialized')
